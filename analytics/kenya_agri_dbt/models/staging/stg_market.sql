fa185b28d3387853dc852947c1044000e61d13d2
jude-craft <derekjude254@gmail.com>
2026-09-11 12:39:10 +0300

    refactor: formatted staging models

--- analytics/kenya_agri_dbt/models/staging/stg_market.sql
+++ analytics/kenya_agri_dbt/models/staging/stg_market.sql
@@ -1,34 +1,36 @@
 with source as (
-    select * from {{source('bronze_layer', 'raw_market')}}
+    select
+        *
+    from
+        {{ source('bronze_layer', 'raw_market') }}
 ),
-
 staged as (
-    select 
+    select
         -- surrogate key
         {{ dbt_utils.generate_surrogate_key(['date','county','market','commodity','unit']) }} as market_id,
-
         -- dimensions
         cast(date as date) as price_date,
         cast(county as string) as county,
         cast(market as string) as market,
         cast(commodity as string) as commodity,
         cast(unit as string) as unit_of_measure,
-
         -- metrics
-        cast(price_kes as float64) as price_kes 
-
-    from source
-
+        cast(price_kes as float64) as price_kes
+    from
+        source
 ),
-
 deduplicated as(
-    select * 
-    from staged 
-    -- BigQuery instance to only keep the first instance of duplicate market_id
-    qualify row_number() over (
-        partition by market_id
-        order by price_kes desc
-    ) = 1
+    select
+        *
+    from
+        staged -- BigQuery instance to only keep the first instance of duplicate market_id
+        qualify row_number() over (
+            partition by market_id
+            order by
+                price_kes desc
+        ) = 1
 )
-
-select * from deduplicated
+select
+    *
+from
+    deduplicated
